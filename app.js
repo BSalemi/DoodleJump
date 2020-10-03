3f857ddee3882ffc11f6c3bd459780180b038a86
Brian <briandsalemi@gmail.com>
2020-10-03 12:13:41 -0400

    Created the start and createPlatforms functions, doodlerLeftSpace, doodlerBottomSpace, isGameOver, and platformCount variables, and added left and bottom styling to doodler

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const grid = document.querySelector('.grid');
-    const doodler = document.createElement('div');
+    const grid = document.querySelector('.grid'),
+          doodler = document.createElement('div');
+    let doodlerLeftSpace = 50,
+        doodlerBottomSpace = 150,
+        isGameOver = false,
+        platformCount = 5
 
     function createDoodler(){
         grid.appendChild(doodler);
         doodler.classList.add('doodler')
+        doodler.style.left = doodlerLeftSpace + 'px'
+        doodler.style.bottom = doodlerBottomSpace + 'px'
     }
 
-    createDoodler()
+    function createPlatforms(){
+        for(let i = 0; i < platformCount; i++){
+            let platGap = 600 / platformCount,
+                newPlatBottom = 100 + i * platGap,
+                newPlatform = new Platform()
+        }
+    }
+
+    function start(){
+        if(!isGameOver){
+            createDoodler()
+            createPlatforms()
+        }
+    }
+
+    //attach to a button
+    start()
 })
